f89d6ea672ac408c125208b7857839665058661f
marwels <mwelfe@gmail.com>
2023-05-08 16:37:09 +0200

    feat: file structure

---
+++ src/weatherAPI.js
@@ -0,0 +1,10 @@
+const key = "e13d20da1c3441cdb5c133607230805";
+
+// URL (required), options (optional)
+fetch("https://url.com/some/url")
+  .then(function (response) {
+    // Successful response :)
+  })
+  .catch(function (err) {
+    // Error :(
+  });
