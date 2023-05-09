cf88e6cfdf9f6211f2ecfa6f4dae7828b0b15eba
marwels <mwelfe@gmail.com>
2023-05-09 17:00:53 +0200

    feat: add search input and onSearch()

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,22 +1,34 @@
-// URL (required), options (optional)
-fetch("https://url.com/some/url")
-  .then(function (response) {
-    // Successful response :)
-  })
-  .catch(function (err) {
-    // Error :(
-  });
+const btn = document.getElementById("fetchingButton");
+const form = document.getElementById("searchForm");
+const input = document.getElementById("search");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
 function fetchWeather(searchFor) {
-  const path = `https://api.weatherapi.com/v1/current.json?key=${key}=${searchFor}`;
+  const path = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${searchFor}`;
   fetch(path, { mode: "cors" })
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      console.log(response.json());
+      console.log(response);
     })
     .catch((err) => {
       console.error(err);
     });
 }
+
+function onSearch(e) {
+  let searchFor;
+  if (input.value !== "") {
+    searchFor = input.value;
+    console.log(searchFor);
+    fetchWeather(searchFor);
+  } else {
+    alert("Type what place you are looking for");
+  }
+}
+
+btn.addEventListener("click", onSearch);
